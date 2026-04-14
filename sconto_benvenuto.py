64397e4f02e70c9dfffc5b80438b50dd8f580569
studcannavale-3a25 <stud.cannavale@gdromagnosi.edu.it>
2026-04-14 09:22:10 +0000

    es1

---
+++ sconto_benvenuto.py
@@ -0,0 +1,8 @@
+def applica_sconto_benvenuto(prezzo_iniziale):
+   
+    return prezzo_iniziale - 5
+
+
+prezzo_scarpe = 80
+prezzo_finale = applica_sconto_benvenuto(prezzo_scarpe)
+print(prezzo_finale)
